c192e3aea58b17fbef809c5d8baff1f536f8f9fc
Cali <hi@cali.so>
2023-07-18 20:36:00 +0800

    fix: middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -30,5 +30,5 @@ export default async function middleware(request: NextRequest) {
 export const config = {
   // Skip all paths that should not be internationalized. This example skips the
   // folders "api", "_next" and all files with an extension (e.g. favicon.ico)
-  matcher: ['/((?!api|_next|_vercel|favicon.ico|assets|studio.*\\..*).*)'],
+  matcher: ['/((?!api|studio|_vercel|_next|.*\\..*).*)'],
 }
